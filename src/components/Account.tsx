85550a9f53188c39f3d9cf3b57c806fe5b43fd06
Pablo The Penguin <pablo12335@gmail.com>
2023-10-16 17:35:23 +0700

    Welcome Cadet

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -5,9 +5,27 @@ export function Account() {
   const { data: ensName } = useEnsName({ address })
 
   return (
-    <div>
-      {ensName ?? address}
-      {ensName ? ` (${address})` : null}
+    <div style={{
+      width: '300px',
+      padding: '20px',
+      backgroundColor: '#1a1a1a',
+      border: '3px solid #4a4a4a',
+      borderRadius: '15px',
+      boxShadow: '0 5px 10px rgba(0, 0, 0, 0.5)',
+      margin: '50px auto'
+    }}>
+      <div className="panel">
+        <div style={{
+          color: '#ffffff',
+          fontSize: '24px',
+          marginBottom: '20px',
+          textAlign: 'center',
+          borderBottom: '2px solid #4a4a4a',
+          paddingBottom: '10px'
+        }} className="cadet-name">{ensName ?? address}</div>
+        {ensName ? ` (${address})` : null}
+      </div>
     </div>
   )
 }
+
---
+++ src/components/CadetNamePanel.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export function CadetNamePanel() {
+  return (
+    <div className="panel">
+      <div className="cadet-name">CADET NAME</div>
+      <div className="row">
+        <span className="label">UMA</span>
+        <span className="value">Cod&bin t ths ory Ufor</span>
+      </div>
+      <div className="row">
+        <span className="label">UMA</span>
+        <span className="value">Cadert you idte</span>
+      </div>
+      <div className="row">
+        <span className="label">MOGBUS</span>
+        <span className="value">SFE</span>
+      </div>
+      <div className="row">
+        <span className="label">Q&OO&JUE</span>
+        <span className="value">CONTROUE</span>
+      </div>
+    </div>
+  );
+};
