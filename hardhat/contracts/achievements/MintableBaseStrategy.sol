045498d77f7984eb033d077822f7835353cda10b
Pablo The Penguin <pablo12335@gmail.com>
2023-10-20 13:30:49 +0700

    abstract Base strategy for easy strategy implementations to mint

---
+++ hardhat/contracts/achievements/MintableBaseStrategy.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.8.13;
+   
+abstract contract Mintable_BaseStrategy {
+    error TxFailedError();
+
+    /**
+         * @dev struct to hold the mint rules for a given token type id
+     */
+    struct MintRules {
+        address[] _targets;
+        string[] _funcs;
+        bytes[] _datas;
+        bytes[] _returns;
+        uint8[] _comparisons;
+        /*
+            0 means equality to msg.sender
+            1 means byte equality
+            2 means <
+            3 means > 
+            4 means <=
+            5 means >=
+        */
+        bool[] identicalDatas;
+        uint8[] _userDatas;
+        string baseURI;
+        uint256 count;
+        bool disabled;
+        bool exists;
+    }
+
+    uint256 public ruleCount; // number of rules
+    mapping(uint256 => MintRules) public mintrules; // numbered rules from id
+
+    function createRule(address, bytes memory, string memory) public {}
+}
